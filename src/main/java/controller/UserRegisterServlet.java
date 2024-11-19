7e5e0524d153ea0d456834c731ae3e51346f7354
dar3valo <wongdarren516@gmail.com>
2024-11-20 06:19:27 +0800

    created new servlet controller for register function

---
+++ src/main/java/controller/UserRegisterServlet.java
@@ -0,0 +1,78 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.UserRegisterDAO;
+import model.UserRegister;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class UserRegisterServlet
+ */
+@WebServlet("/UserRegisterServlet")
+public class UserRegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserRegisterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		doGet(request, response);
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String confirmPassword = request.getParameter("confirmPassword");
+		String gender = request.getParameter("gender");
+		
+		if (!password.equals(confirmPassword)) {
+	        request.setAttribute("errorMessage", "Passwords do not match!");
+	        request.getRequestDispatcher("register.jsp").forward(request, response);
+	        return; // Exit early to prevent further processing
+	    }
+		
+		try {
+			UserRegisterDAO userRegisterDAO = new UserRegisterDAO();
+			
+			if(userRegisterDAO.isEmailExist(email)) {
+				request.setAttribute("errorMessage", "Email already exists, please key in a different email...");
+				request.getRequestDispatcher("register.jsp").forward(request, response);
+			}else {
+				UserRegister insertUser = userRegisterDAO.insertUserInfo(name, email, password, gender);
+				
+				if(insertUser != null) {
+					response.sendRedirect(request.getContextPath() + "/public/HTML/login.jsp");
+				}else {
+					request.setAttribute("errorMessage", "An unexpected error occured ;-;");
+					request.getRequestDispatcher("register.jsp").forward(request, response);
+				}
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+			request.setAttribute("errorMessage", "An unexpected error occured ;-;");
+			request.getRequestDispatcher("register.jsp").forward(request, response);
+		}
+	}
+
+}
