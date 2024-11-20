40e4eb3025f71efb6666153d97aa26cab9bbdacf
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2024-11-21 02:56:38 +0800

    method to handle get all service info

--- src/main/java/model/ServiceDAO.java
+++ src/main/java/model/ServiceDAO.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.sql.*;
 
 public class ServiceDAO {
+	// get all service (name, id)
 	public static List<Service> getAllServices(){
 		List<Service> services = new ArrayList<>();
 		Connection conn = null;
@@ -42,6 +43,45 @@ public class ServiceDAO {
 				e.printStackTrace();
 			}
 		}
+		return services;
+	}
+	
+	// get all service (all information)
+	public static List<Service> getAllServiceInformation(int serviceCategoryId) {
+		List<Service> services = new ArrayList<>();
+		try {
+			// config
+			Class.forName("org.postgresql.Driver");
+			String dbUrl = "jdbc:postgresql://ep-shiny-queen-a5kntisz.us-east-2.aws.neon.tech/neondb?sslmode=require";
+			Connection connection = DriverManager.getConnection(dbUrl, "neondb_owner", "mMGl0ndLNXD6");
+
+			// query
+			String ps = "SELECT s.service_id, s.name, s.description, s.price, s.service_photo_url" +
+			             "FROM Service AS s " +
+			             "INNER JOIN Service_Service_Category AS ssc " +
+			             "ON s.service_id = ssc.service_id " +
+			             "WHERE ssc.service_category_id = ?";
+			
+			PreparedStatement statement = connection.prepareStatement(ps);
+			statement.setInt(1, serviceCategoryId);
+			ResultSet rs = statement.executeQuery();
+
+			while(rs.next()) {
+				int serviceId = rs.getInt("service_id");
+				String serviceName = rs.getString("name");
+				String serviceDescription = rs.getString("description");
+				double servicePrice = rs.getDouble("price");
+				String serviceImage = rs.getString("service_photo_url");
+				
+				services.add(new Service(serviceId, serviceName, serviceDescription, servicePrice, serviceImage));
+			}
+			
+			connection.close();
+			
+		} catch (Exception e) {
+			 e.printStackTrace(); 
+		}
+		
 		return services;
 	}
 }
