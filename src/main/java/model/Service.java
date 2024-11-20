72f83aa7a4a5c7b634289036a5ec7782f442b422
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2024-11-21 02:53:42 +0800

    added more methods for handling service class construction

--- src/main/java/model/Service.java
+++ src/main/java/model/Service.java
@@ -3,11 +3,28 @@ package model;
 public class Service {
 	private int service_id;
 	private String name;
+	private String description;
+	private double price;
+	private String service_photo_url;
 	
+	// constructor where all values are passed
+	public Service(int service_id, String name, String description, double price, String service_photo_url) {
+		super();
+		this.service_id = service_id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.service_photo_url = service_photo_url;
+	}
+	
+	// provide default values if not specified (only id and name needs to be specified)
 	public Service(int service_id, String name) {
 		super();
 		this.service_id = service_id;
 		this.name = name;
+		this.description = "";
+		this.price = 0.0;
+		this.service_photo_url = "../Image/defaultpic.png";
 	}
 
 	public int getService_id() {
