87e0a61d6f1283adb4fb8396d20f0156d1970e0e
dar3valo <wongdarren516@gmail.com>
2024-11-21 00:18:11 +0800

    Modified ServiceDAO to retrieve data from the database to the dropdown list

--- src/main/java/model/ServiceDAO.java
+++ src/main/java/model/ServiceDAO.java
@@ -1,14 +1,47 @@
 package model;
 import java.util.List;
 import java.util.ArrayList;
+import java.sql.*;
 
 public class ServiceDAO {
 	public static List<Service> getAllServices(){
 		List<Service> services = new ArrayList<>();
-		services.add(new Service(1, "Home"));
-		services.add(new Service(2, "Office"));
-		services.add(new Service(3, "Shower"));
-		
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			String dbUrl = "jdbc:postgresql://ep-shiny-queen-a5kntisz.us-east-2.aws.neon.tech/neondb?sslmode=require";
+			conn = DriverManager.getConnection(dbUrl, "neondb_owner", "mMGl0ndLNXD6");
+			String sql = "SELECT service_id, name FROM Service";
+			
+			stmt = conn.prepareStatement(sql);
+			
+			rs = stmt.executeQuery();
+
+			while(rs.next()) {
+				int serviceId = rs.getInt("service_id");
+				String serviceName = rs.getString("name");
+				
+				services.add(new Service(serviceId, serviceName));
+			}
+		}catch(Exception e) {
+			 e.printStackTrace(); 
+		}finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(stmt != null) {
+					stmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
 		return services;
 	}
 }
