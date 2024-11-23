8f3a7228bc195182298ef2d392ed47e09a4e57da
dar3valo <wongdarren516@gmail.com>
2024-11-24 00:40:29 +0800

    Created cart item value bean (incompleted)

---
+++ src/main/java/model/CartItem.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.sql.Date;
+
+public class CartItem {
+	private Date booking_date;
+	private String special_request;
+	private String main_address;
+	private int postal_code;
+}
