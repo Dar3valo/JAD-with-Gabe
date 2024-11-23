3593454a7d9644fa4ba4123b1b832a7a50e92fa7
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2024-11-23 23:56:03 +0800

    backend for updating

--- src/main/java/controller/EditServiceServlet.java
+++ src/main/java/controller/EditServiceServlet.java
@@ -17,60 +17,95 @@ import java.io.IOException;
 @WebServlet("/EditServiceServlet")
 public class EditServiceServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public EditServiceServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public EditServiceServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		try {
-			String input_addServicePrice = request.getParameter("addServicePrice");
-			
-			String addServiceName = request.getParameter("addServiceName");
-			String addServiceDescription = request.getParameter("addServiceDescription");
-			double addServicePrice = Double.parseDouble(input_addServicePrice);
-			String addServicePhotoUrl = "../Image/carouselImage1.jpg";
 
+		try {
+			String formAction = request.getParameter("action");
+			
 			// general config
 			int rowsAffected = 0;
 			
-			rowsAffected = ServiceDAO.createService(addServiceName, addServiceDescription, addServicePrice, addServicePhotoUrl);
-			
-			if (rowsAffected == 0) {
-				throw new Exception("For some reason no rows changed :| maybe theres no data associated with the id? here is the information given in request:" +
-			"\naddServiceName: " + addServiceName +
-			"\naddServiceDescription: " + addServiceDescription +
-			"\naddServicePrice: " + addServicePrice + 
-			"\naddServicePhotoUrl: " + addServicePhotoUrl + "\n");
+			if (formAction.equals("create")) {
+				String input_addServicePrice = request.getParameter("addServicePrice");
+
+				String addServiceName = request.getParameter("addServiceName");
+				String addServiceDescription = request.getParameter("addServiceDescription");
+				double addServicePrice = Double.parseDouble(input_addServicePrice != null ? input_addServicePrice : "0");
+				String addServicePhotoUrl = "../Image/carouselImage1.jpg";
+
+				rowsAffected = ServiceDAO.createService(addServiceName, addServiceDescription, addServicePrice,
+						addServicePhotoUrl);
+				
+				if (rowsAffected == 0) {
+					throw new Exception(
+							"For some reason no rows changed :| maybe theres no data associated with the id? here is the information given in request:"
+									+ "\naddServiceName: " + addServiceName + "\naddServiceDescription: "
+									+ addServiceDescription + "\naddServicePrice: " + addServicePrice
+									+ "\naddServicePhotoUrl: " + addServicePhotoUrl + "\n");
+				}
+				
+			} else if (formAction.equals("update")) {
+				// update service data
+				String input_serviceId = request.getParameter("serviceId");
+				String input_servicePrice = request.getParameter("servicePrice");
+				
+				int serviceId = Integer.parseInt(input_serviceId != null ? input_serviceId : "0");
+				double servicePrice = Double.parseDouble(input_servicePrice != null ? input_servicePrice : "0");
+				String serviceName = request.getParameter("serviceName");
+				String serviceDescription = request.getParameter("serviceDescription");
+				String addServicePhotoUrl = "../Image/carouselImage1.jpg";
+				
+				rowsAffected = ServiceDAO.updateServiceById(serviceId, serviceName, serviceDescription, servicePrice, addServicePhotoUrl);
+				
+				if (rowsAffected == 0) {
+					throw new Exception("For some reason no rows changed :| maybe theres no data associated with the id? here is the information given in request:" +
+				"\nserviceId: " + serviceId +
+				"\nserviceName: " + serviceName +
+				"\nserviceDescription: " + serviceDescription +
+				"\nservicePrice: " + servicePrice +
+				"\naddServicePhotoUrl: " + addServicePhotoUrl + "\n");
+				}
+				
+			} else {
+                throw new Exception("No appropriate associated form action found in parameter. \nform action given: " + formAction);
 			}
 			
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/GetServiceInformationServlet?serviceCategory=0");
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher("/GetServiceInformationServlet?serviceCategory=0");
 			dispatcher.forward(request, response);
 			return;
-			
-		} catch (Exception e){
-            e.printStackTrace();
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/GetServiceInformationServlet?serviceCategory=0");
-    		dispatcher.forward(request, response);
-    		return;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher("/GetServiceInformationServlet?serviceCategory=0");
+			dispatcher.forward(request, response);
+			return;
 		}
 	}
-
 }
