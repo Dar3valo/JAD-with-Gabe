2c2639b705fc3f56efe6eaa221d5ab42f8d07e3a
dar3valo <wongdarren516@gmail.com>
2024-11-24 00:39:51 +0800

    Controller logic for schedule timings

---
+++ src/main/java/controller/ScheduleDropdownServlet.java
@@ -0,0 +1,60 @@
+package controller;
+
+import jakarta.servlet.ServletException; 
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Schedule;
+import model.ScheduleDAO;
+import model.Service;
+import model.ServiceDAO;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Servlet implementation class ScheduleDropdownServlet
+ */
+@WebServlet("/ScheduleDropdownServlet")
+public class ScheduleDropdownServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ScheduleDropdownServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		try {
+			List<Schedule> scheduleDropdown = ScheduleDAO.getAllSchedule();
+			
+			// Store services in session
+            HttpSession session = request.getSession();
+            session.setAttribute("scheduleTimings", scheduleDropdown);
+            
+            response.sendRedirect(request.getContextPath() + "/public/HTML/booking.jsp");
+		}catch(Exception e) {
+			e.printStackTrace();
+			request.setAttribute("errorMessage", "An unexpected error occurred. Please try again.");
+			response.sendRedirect(request.getContextPath() + "error.jsp");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
